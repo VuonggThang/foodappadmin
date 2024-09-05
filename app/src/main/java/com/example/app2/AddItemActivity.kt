ac62d39fb16f88134f793e42614ab1790757ea28
M4800 <vuonggthang@gmail.com>
2024-09-05 20:39:04 +0700

    commit app admin 5/9

--- app/src/main/java/com/example/app2/AddItemActivity.kt
+++ app/src/main/java/com/example/app2/AddItemActivity.kt
@@ -2,10 +2,30 @@ package com.example.app2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.app2.databinding.ActivityAddItemBinding
 
 class AddItemActivity : AppCompatActivity() {
+    private val binding : ActivityAddItemBinding by lazy {
+        ActivityAddItemBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_item)
+        setContentView(binding.root)
+
+        binding.selectedIamge.setOnClickListener {
+            pickImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+        binding.backimageButton.setOnClickListener {
+            finish()
+        }
+
+    }
+    val pickImage = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){ uri ->
+        if (uri !=null){
+            binding.selectedIamge.setImageURI(uri)
+        }
+
     }
 }
--- app/src/main/java/com/example/app2/MainActivity.kt
+++ app/src/main/java/com/example/app2/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.app2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.app2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding:ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.addMenu.setOnClickListener{
+            val intent = Intent(this,AddItemActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
