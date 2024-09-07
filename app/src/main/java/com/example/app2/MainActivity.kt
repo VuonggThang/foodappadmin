f33b3c0f3b7c7d631854a20670e1eda2214d4434
M4800 <vuonggthang@gmail.com>
2024-09-07 11:41:02 +0700

    commit app admin 7/9

--- app/src/main/java/com/example/app2/MainActivity.kt
+++ app/src/main/java/com/example/app2/MainActivity.kt
@@ -6,16 +6,20 @@ import android.os.Bundle
 import com.example.app2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val binding:ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.addMenu.setOnClickListener{
-            val intent = Intent(this,AddItemActivity::class.java)
+        binding.addMenu.setOnClickListener {
+            val intent = Intent(this, AddItemActivity::class.java)
             startActivity(intent)
         }
-        binding.allItemMenu.setOnClickListener{
-            val intent = Intent(this,AllItemActivity::class.java)
+        binding.allItemMenu.setOnClickListener {
+            val intent = Intent(this, AllItemActivity::class.java)
+            startActivity(intent)
+        }
+        binding.outForDeliveryButton.setOnClickListener {
+            val intent = Intent(this, OutForDeliveryActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/app2/OutForDeliveryActivity.kt
+++ app/src/main/java/com/example/app2/OutForDeliveryActivity.kt
@@ -2,10 +2,30 @@ package com.example.app2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.app2.adapter.DeliveryAdapter
+import com.example.app2.databinding.ActivityOutForDeliveryBinding
 
 class OutForDeliveryActivity : AppCompatActivity() {
+    private val binding : ActivityOutForDeliveryBinding by lazy{
+        ActivityOutForDeliveryBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_out_for_delivery)
+        setContentView(binding.root)
+        val customerName = arrayListOf(
+            "Thang",
+            "Mai",
+            "Anh",
+        )
+        val moneyStatus = arrayListOf(
+            "Nhận",
+            "Chưa nhận",
+            "Đang đợi",
+        )
+        val adapter = DeliveryAdapter(customerName,moneyStatus)
+        binding.deliveryRecyclerView.adapter = adapter
+        binding.deliveryRecyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
