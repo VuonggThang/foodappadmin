b7a258d755f79d249f9011a79ecd8fcc3f513a70
M4800 <vuonggthang@gmail.com>
2024-09-14 05:04:39 +0700

    commit app admin 14/9 lan1

--- app/src/main/java/com/example/app2/AddItemActivity.kt
+++ app/src/main/java/com/example/app2/AddItemActivity.kt
@@ -1,30 +1,108 @@
 package com.example.app2
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.app2.databinding.ActivityAddItemBinding
+import com.example.app2.model.AllMenu
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import java.sql.DatabaseMetaData
 
 class AddItemActivity : AppCompatActivity() {
+    //food item details
+    private lateinit var foodName: String
+    private lateinit var foodPrice: String
+    private lateinit var foodDescription: String
+    private lateinit var foodIngredient: String
+    private  var foodImageUri: Uri?= null
+    //firebase
+    private lateinit var auth:FirebaseAuth
+    private lateinit var database:FirebaseDatabase
+
     private val binding : ActivityAddItemBinding by lazy {
         ActivityAddItemBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        //Initialize Firebase
+        auth = FirebaseAuth.getInstance()
+        //Initialize Firebase database instance
+        database = FirebaseDatabase.getInstance()
 
+        binding.AddItemButton.setOnClickListener{
+            //get data from filed
+            foodName = binding.foodName.text.toString().trim()
+            foodPrice = binding.foodPrice.text.toString().trim()
+            foodDescription = binding.description.text.toString().trim()
+            foodIngredient = binding.ingredint.text.toString().trim()
+            if(!(foodName.isBlank()||foodPrice.isBlank()||foodDescription.isBlank()||foodIngredient.isBlank())){
+                uploadData()
+                Toast.makeText(this,"Thêm item thành công",Toast.LENGTH_SHORT).show()
+                finish()
+            }else
+                Toast.makeText(this,"Điền đầy đủ thông tin",Toast.LENGTH_SHORT).show()
+
+        }
         binding.selectedIamge.setOnClickListener {
-            pickImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+            pickImage.launch("image/*")
         }
         binding.backimageButton.setOnClickListener {
             finish()
         }
 
     }
-    val pickImage = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){ uri ->
+
+    private fun uploadData() {
+        //get a reference to the "menu" node in the database
+        val menuRef = database.getReference("menu")
+        //genrate a unique key for the  new menu item
+        val newItemKey = menuRef.push().key
+        if (foodImageUri!= null){
+            val storageRef = FirebaseStorage.getInstance().reference
+            val imageRef = storageRef.child("menu_images/${newItemKey}.jpg")
+            val uploadTask = imageRef.putFile(foodImageUri!!)
+            uploadTask.addOnSuccessListener {
+                imageRef.downloadUrl.addOnSuccessListener {
+                    downloadUrl ->
+                    // create a new menu item
+                    val newItem = AllMenu(
+                        foodName = foodName,
+                        foodPrice = foodPrice,
+                        foodDescription = foodDescription,
+                        foodIngredient = foodIngredient,
+                        foodImage = downloadUrl.toString(),
+                    )
+                    newItemKey?.let {
+                        key->
+                        menuRef.child(key).setValue(newItem).addOnSuccessListener {
+                            Toast.makeText(this,"dữ liệu tải lên thành công",Toast.LENGTH_SHORT).show()
+                        }
+                            .addOnFailureListener {
+                                Toast.makeText(this,"dữ liệu tải lên không thành công",Toast.LENGTH_SHORT).show()
+                            }
+                    }
+                }
+
+            }.addOnFailureListener {
+                Toast.makeText(this,"Image tải không thành công",Toast.LENGTH_SHORT).show()
+            }
+        }else {
+            Toast.makeText(this,"Hãy chọn ảnh",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val pickImage = registerForActivityResult(ActivityResultContracts.GetContent()){ uri ->
         if (uri != null){
             binding.selectedIamge.setImageURI(uri)
+            foodImageUri = uri
         }
 
     }
--- app/src/main/java/com/example/app2/LoginActivity.kt
+++ app/src/main/java/com/example/app2/LoginActivity.kt
@@ -1,23 +1,34 @@
 package com.example.app2
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.example.app2.databinding.ActivityLoginBinding
 import com.example.app2.model.UserModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.database
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var email:String
+    private var userName: String? = null
+    private var nameOfRestaurant: String? = null
+    private lateinit var email: String
     private lateinit var password: String
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     private val binding: ActivityLoginBinding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
@@ -29,16 +40,26 @@ class LoginActivity : AppCompatActivity() {
         auth = Firebase.auth
         // initialize Firebase database
         database = Firebase.database.reference
+        // initialize google sign in
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
         binding.loginbutton.setOnClickListener {
             // get text form edit text
             email = binding.email.text.toString().trim()
             password = binding.password.text.toString().trim()
-            if (email.isBlank()||password.isBlank()) {
-                Toast.makeText( this,  "hay dien day du thong tin", Toast.LENGTH_SHORT).show()
-            }else{
+            if (email.isBlank() || password.isBlank()) {
+                Toast.makeText(this, "hay dien day du thong tin", Toast.LENGTH_SHORT).show()
+            } else {
                 createUserAccount(email, password)
             }
-
+        }
+        binding.googleButton.setOnClickListener{
+            val signIntent = googleSignInClient.signInIntent
+            launcher.launch(signIntent)
         }
         binding.donthaveaccount.setOnClickListener {
             val intent = Intent(this, SignActivity::class.java)
@@ -47,18 +68,24 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun createUserAccount(email: String, password: String) {
-        auth.signInWithEmailAndPassword (email, password).addOnCompleteListener { task ->
-            if (task.isSuccessful){
-                val user: FirebaseUser? = auth.currentUser
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val user = auth.currentUser
+                Toast.makeText(this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show()
                 updateUi(user)
-            }else{
-                auth.createUserWithEmailAndPassword (email, password).addOnCompleteListener { task ->
+            } else {
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        val user: FirebaseUser? = auth.currentUser
+                        val user = auth.currentUser
+                        Toast.makeText(
+                            this,
+                            "Tạo người dùng và đăng nhập thành công",
+                            Toast.LENGTH_SHORT
+                        ).show()
                         saveUserData()
                         updateUi(user)
-                    }else{
-                        Toast.makeText(this,"Xac thuc khong thanh cong",Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Xac thuc khong thanh cong", Toast.LENGTH_SHORT).show()
                         Log.d("Account", "Tao tai khoan nguoi dung: that bai", task.exception)
                     }
                 }
@@ -69,15 +96,51 @@ class LoginActivity : AppCompatActivity() {
     private fun saveUserData() {
         email = binding.email.text.toString().trim()
         password = binding.password.text.toString().trim()
-        val user = UserModel(email,password)
+        val user = UserModel(userName, nameOfRestaurant, email, password)
         val userId = FirebaseAuth.getInstance().currentUser?.uid
         userId?.let {
             database.child("user").child(it).setValue(user)
         }
     }
 
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            if (task.isSuccessful) {
+                val account :GoogleSignInAccount= task.result
+                val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+                auth.signInWithCredential(credential).addOnCompleteListener{
+                    authTask ->
+                    if (authTask.isSuccessful){
+                        //successfully sign in with google
+                        Toast.makeText(this,"Đăng nhập thành công với google",Toast.LENGTH_SHORT).show()
+                        updateUi(authTask.result?.user)
+                        finish()
+//                        startActivity(Intent(this,MainActivity::class.java))
+                    }else{
+                        Toast.makeText(this,"Đăng nhập không thành công với google",Toast.LENGTH_SHORT).show()
+                        Log.e("GoogleSignIn", "signInResult:failed code=" + authTask.exception)
+                    }
+                }
+            }
+            else{
+                Toast.makeText(this,"Đăng nhập không thành công với google",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    //Check if user is already logged in
+    override fun onStart(){
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser!=null) {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+        }
+    }
     private fun updateUi(user: FirebaseUser?) {
-        startActivity(Intent ( this, MainActivity::class.java))
+        startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
 }
